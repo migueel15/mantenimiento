52cd6da9e275214407ca04ac0028b5593fb1ca34
saalma4 <160852812+saalma4@users.noreply.github.com>
2025-03-17 22:28:45 +0100

    test excepcion

--- practica1/src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
+++ practica1/src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
@@ -15,14 +15,24 @@ public class ClubDeportivoAltoRendimientoTest {
     }*/
 
     @Test
-    @DisplayName("test que comprueba que se crea una actividad")
     public void anyadirActividad_test() throws ClubException {
 
         clubDeportivoAltoRendimiento = new ClubDeportivoAltoRendimiento("clubDeportivoAltoRendimiento", 10, 10.0);
         int plazasIniciales = clubDeportivoAltoRendimiento.plazasLibres("Actividad 1");
-        String[] datos = {"Actividad 1", "Descripcion 1", "10", "5", "10.0"};
+        String[] datos = {"abc", "Actividad 1", "10", "5", "10.0"};
         clubDeportivoAltoRendimiento.anyadirActividad(datos);
         int plazasNuevas = clubDeportivoAltoRendimiento.plazasLibres("Actividad 1");
+        System.out.println(plazasIniciales + " " + plazasNuevas);
         assertNotEquals(plazasIniciales, plazasNuevas);
     }
+    @Test
+    public void ClubDeportivoAltoRendimientoNZeroOrLessExpectException() throws ClubException {
+        assertThrows(ClubException.class, () -> {
+            clubDeportivoAltoRendimiento = new ClubDeportivoAltoRendimiento("clubDeportivoAltoRendimiento", 10, -10.0);
+        });
+    }
+    @Test
+
+
+
 }
