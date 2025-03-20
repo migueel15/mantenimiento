e5d7b94fd85530a799ecd312457fef8d216a6b38
migueel15 <miguelangeldorado10@gmail.com>
2025-03-20 11:14:40 +0100

    add base

---
+++ practica2/src/test/java/org/mps/tree/BinarySearchTreeTest.java
@@ -0,0 +1,16 @@
+// Salma Boulagna Moreno
+// Miguel Ángel Dorado Maldonado
+
+package org.mps.tree;
+
+import org.junit.jupiter.api.*;
+import org.junit.platform.engine.TestTag;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * BinarySearchTreeTest
+ */
+public class BinarySearchTreeTest {
+
+}
