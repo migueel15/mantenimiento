f0465e8883525fa2f2d17ff37bb7244bc3020176
migueel15 <miguelangeldorado10@gmail.com>
2025-05-06 13:07:46 +0200

    quitar dependencias no usadas

--- practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
+++ practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -1,6 +1,5 @@
 package org.mps.boundedqueue;
 
-import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
